c577d1eecfc12edde99597aa5d4f720a57a148ed
ltillier <laurel@tillier.us>
2026-09-23 19:58:14 -0400

    Completed Steps 2,3 variables challenge
    made mr furious get red over time, made background color change to dark blue, constrained so that it doesn't get tooo dark

--- variablesChallenge/js/script.js
+++ variablesChallenge/js/script.js
@@ -1,24 +1,67 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  }
+};
+// lets not forget the sky either
+let sky = {
+    r:160,
+    g:180,
+    b:200
 
 }
 
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
+  background(sky.r, sky.g, sky.b);
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
+
+  // Make Mr. Furious redder over time
+  mrFurious.fill.g = mrFurious.fill.g-1
+  mrFurious.fill.b = mrFurious.fill.b-1
+ mrFurious.fill.g=constrain(mrFurious.fill.g,50,255);
+
+ // Make day turn to night
+sky.r=sky.r-1
+sky.g=sky.g-1
+sky.b=sky.b-1
+
+// Make night not so dark
+sky.r=constrain(sky.r,15,255);
+sky.g=constrain(sky.g,50,255);
+sky.b=constrain(sky.b,100,255);
+
 
 }
